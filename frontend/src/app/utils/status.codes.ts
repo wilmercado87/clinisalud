5cc4113914e28627a9bd17a47264c602fdfbee36
William Mercado <wishi8704@hotmail.com>
2026-05-03 22:54:57 -0500

    chore: unificar repositorio eliminando git anidado en frontend

--- frontend/src/app/utils/status.codes.ts
+++
@@ -1,9 +0,0 @@
-export const ERROR_MAPPING: Record<number, string> = {
-  401: 'Credenciales inválidas. Revise su correo',
-  402: 'Credenciales inválidas. Revise su contraseña',
-  403: 'El usuario se encuentra inactivo',
-  404: 'Servidor no encontrado',
-  405: 'Email ya existe',
-  406: 'DNI ya existe',
-  500: 'Error interno del servidor'
-};
